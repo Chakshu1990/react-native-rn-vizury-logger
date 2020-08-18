3b4342dc380d7b8a61a0f32efa8330fc8df4b2a0
Chakshu <chakshu.arora@affle.com>
2020-08-18 15:25:52 +0530

    With VizuryEvent Logger Framework Added.

--- ios/RnVizuryLogger.m
+++ ios/RnVizuryLogger.m
@@ -1,4 +1,5 @@
 #import "RnVizuryLogger.h"
+#import <VizuryEventLogger/VizuryEventLogger.h>
 
 @implementation RnVizuryLogger
 
@@ -11,6 +12,21 @@ RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnu
 }
 RCT_EXPORT_METHOD(logVE360:(NSDictionary *)param param2:(NSString *)param2) {
   NSLog(@"Welcome, %@", param);
+    [VizuryEventLogger logEvent:param2 WithAttributes:param];
+}
+
+RCT_EXPORT_METHOD(passFCMToken:(NSString *)recieveFCMTOken)
+{
+  NSData *data = [recieveFCMTOken dataUsingEncoding:NSUTF8StringEncoding];
+  [VizuryEventLogger registerForPushWithToken:data];
+  RCTLogInfo(@"Pretending to create an event %@",recieveFCMTOken);
+  recivedFCMTOKEN = recieveFCMTOken;
+}
+
+RCT_EXPORT_METHOD(rejectionHandle:(NSString *)rejectRecieve)
+{
+  [VizuryEventLogger didFailToRegisterForPush];
+  RCTLogInfo(@"Pretending to create an event %@",rejectRecieve);
 }
 
 @end
